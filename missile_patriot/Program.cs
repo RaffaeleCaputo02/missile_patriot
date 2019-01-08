cc4665f5e13253c21956e61cd7a571b0c60cea86
rcaputo95 <rcaputo95@gmail.com>
2019-01-08 17:37:22 +0100

    ho chiesto in imput in decimi il tempo di boot di caricamneto del sistema

---
+++ missile_patriot/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace missile_patriot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tempo_caricamento;
+            Console.WriteLine("Inserisci il tempo di caricamento misurato in decimi");
+            tempo_caricamento = Convert.ToInt32(Console.ReadLine());
+
+           
+
+
+        }
+    }
+}
